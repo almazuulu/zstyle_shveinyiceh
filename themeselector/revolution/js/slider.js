75ce9a458db9291342dddc2ea4fe546fadd22a9a
System Administrator <root@users-MBP.N4>
2020-11-30 23:49:23 +0600

    first commit to zstyle

---
+++ themeselector/revolution/js/slider.js
@@ -0,0 +1,48 @@
+/*------------------------------------------------------------------------------*/
+/*  Home_Page Slider
+/*------------------------------------------------------------------------------*/
+
+var revapi1,
+  tpj;
+jQuery(function() {
+  tpj = jQuery;
+  if(tpj("#rev_slider_1_1").revolution == undefined){
+    revslider_showDoubleJqueryError("#rev_slider_1_1");
+  }else{
+    revapi1 = tpj("#rev_slider_1_1").show().revolution({
+      sliderLayout:"fullwidth",
+      visibilityLevels:"1240,1024,778,480",
+      gridwidth:"1240",
+      gridheight:"700",
+      minHeight:"",
+      spinner:"spinner0",
+      editorheight:"705,768,450,350",
+      responsiveLevels:"1240,1240,778,480",
+      disableProgressBar:"on",
+      navigation: {
+        mouseScrollNavigation:false,
+        onHoverStop:false,
+        touch: {
+          touchenabled:true
+        },
+        arrows: {
+          enable:false,
+          style:"custom",
+          hide_onmobile:true,
+          hide_under:768,
+          hide_onleave:true,
+          left: {
+
+          },
+          right: {
+
+          }
+        }
+      },
+      fallbacks: {
+        allowHTML5AutoPlayOnAndroid:true,
+      },
+    });
+  }
+  
+});
